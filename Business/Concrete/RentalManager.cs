13b6696efc63a1990961834dc2f88244c808e039
omer akkaya <omer-akkaya@outlook.com>
2022-07-21 03:11:48 +0300

    Bussiness Layers Updated

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstract;
+using Core.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+        public IResult Add(Rental rental)
+        {
+            foreach (var item in _rentalDal.GetAll())
+            {
+                if(item.ReturnDate>rental.RentDate && item.CarId==rental.CarId)
+                {
+                    return new ErrorResult("Kiralama işlemi başarısız");
+                }
+            }
+            _rentalDal.Add(rental);
+            return new SuccessResult("Kiralama işlemi başarılı");
+
+            
+            
+             
+
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult("Araba kiralama silindi");
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult("Araba kiralanma süreci güncellendi");
+        }
+    }
+}
